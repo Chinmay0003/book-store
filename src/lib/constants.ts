4cd714e560dc9bbdc74071da8cd719b4f1718f5e
ChinmayPPP <chinmay@pinegap.us>
2025-05-08 02:53:43 +0530

    adwsf

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
-//export const BACKEND_API = "https://api.mybestkid.in";
-export const BACKEND_API = "http://localhost:4000";
+export const BACKEND_API = "https://api.mybestkid.in";
+// export const BACKEND_API = "http://localhost:4000";
